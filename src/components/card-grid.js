13d8f9893dbb40ae9403ed4b81968fd5afb4cff6
Dylan <nielsen.dylan@gmail.com>
2018-03-13 09:35:03 -0700

    Add active square display

--- src/components/card-grid.js
+++ src/components/card-grid.js
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react';
 import './card-grid.css';
 
 export default function CardGrid(props) {
-
   /**
    * Generates the entire grid by calling generate row for 7 rows.
    * Returns an html table.
@@ -14,51 +13,53 @@ export default function CardGrid(props) {
     }
     return (
       <table className="grid">
-        <tbody>
-          {grid}
-        </tbody>
+        <tbody>{grid}</tbody>
       </table>
-    )
+    );
   }
 
   /**
-   * 
+   *
    * @param {Number} row
-   * 
+   *
    * Generates a row that will fill each cell with the proper completion.
    * Will also generate the available cell component for the cell that is allowed to be completed.
-   *  
+   *
    */
   function generateRow(row) {
     const cells = [];
-    
+
     for (let i = 0; i < 7; i++) {
       let cell;
       if (props.xArray[row * 7 + i] === true) {
         cell = (
           <div className="complete" key={row * 7 + i}>
             X
-          </div>)
+          </div>
+        );
       } else if (props.xArray[row * 7 + i] === false) {
         cell = (
           <div className="incomplete" key={row * 7 + i}>
             O
           </div>
-        )
+        );
+      } else if (
+        // make the next available square active
+        props.xArray[row * 7 + i - 1] !== undefined &&
+        (props.xArray[row * 7 + i - 2] !== undefined || row * 7 + i - 2 <= 0)
+      ) {
+        cell = <div className="active" key={row * 7 + i} />;
       } else {
         cell = <div className="pending" key={row * 7 + i} />;
       }
       cells.push(
         <td key={row * 7 + i} className="grid-square">
           {cell}
-        </td>);
+        </td>
+      );
     }
-    return (
-      <tr key={row}>{cells}</tr>
-    )
+    return <tr key={row}>{cells}</tr>;
   }
 
-  return (
-    generateGrid()
-  )
+  return generateGrid();
 }
