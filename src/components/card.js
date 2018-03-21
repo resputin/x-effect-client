2433fa1d086c9e66cebea1e9c81792352350b22a
Dylan <nielsen.dylan@gmail.com>
2018-03-21 16:26:02 -0700

    Add react-accordion and clean up notification form

--- src/components/card.js
+++ src/components/card.js
@@ -3,18 +3,32 @@ import { connect } from "react-redux";
 import CardGrid from './card-grid'
 import './card.css';
 import { checkX } from "../actions/card";
-// import NotificationForm from "./notification-form";
+import NotificationForm from "./notification-form";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/minimal-example.css';
 
 export function Card(props) {
 
   return <div className="card">
       <h2 className="title">{props.cards[props.id].name}</h2>
-      {/* <button className="settings">
-        <img src="http://moziru.com/images/gears-clipart-transparent-background-19.png" alt="settings" />
-      </button> */}
       <div className="grid-container" />
-      {/* <NotificationForm card={props.cards[props.id]} form={`NotificationForm_${props.id}`} initialValues={{ cardId: props.id, minutesUntilExpires: 720 }} /> */}
+
       <CardGrid card={props.cards[props.id]} onCheck={id => props.dispatch(checkX(id))} />
+      <Accordion>
+        <AccordionItem>
+          <AccordionItemTitle>
+            <h3>{'\u25BC'} Notification Settings</h3>
+          </AccordionItemTitle>
+          <AccordionItemBody>
+            <NotificationForm card={props.cards[props.id]} form={`NotificationForm_${props.id}`} initialValues={{ cardId: props.id, minutesUntilExpires: 720 }} />
+          </AccordionItemBody>
+        </AccordionItem>
+      </Accordion>
     </div>;
 }
 
--- src/components/notification-form.js
+++ src/components/notification-form.js
@@ -10,17 +10,18 @@ import {
 import Input from './input';
 import './signup-form.css';
 import { setNotification } from "../actions/notification";
-import TimePicker from 'react-bootstrap-time-picker';
 const messageLength = length({ min: 1, max: 160 });
 
 export function NotificationForm(props) {
   function onSubmit(values) {
     console.log(values);
-    const { sendTo, message, minutesUntilExpires, cardId } = values;
+    const { sendTo, message, sendTime, cardId } = values;
+    const a = sendTime.split(':');
+    const minutesUntilExpires = Math.abs(+a[0] * 60 + +a[1] - 1440);
     const notificationSettings = { sendTo, message, minutesUntilExpires, cardId };
     console.log(notificationSettings);
-    return props.dispatch(setNotification(values))
-      .then(response => console.log(response));
+    // return props.dispatch(setNotification(values))
+    //   .then(response => console.log(response));
   }
 
   let errorMessage;
@@ -34,7 +35,7 @@ export function NotificationForm(props) {
       {errorMessage}
       <Field component={Input} type="text" name="sendTo" label="Phone Number" validate={[required, nonEmpty, isTrimmed, isPhoneNumber]} />
       <Field component={Input} type="text" name="message" label="What do you want your text to say" validate={[required, nonEmpty, isTrimmed, messageLength]} />
-      <Field component={TimePicker} name="hours" label="Time"/>
+      <Field component={Input} type="time" name="sendTime" label="What time would you like to be notified" validate={[required]} />
       <button type="submit" className="signup-button" onChange={value => props.onChange(value)}>
         Turn on Notifications
       </button>
